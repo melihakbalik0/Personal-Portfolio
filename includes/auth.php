ced28ee882f4726974c78eed757f6b4b36489fbe
melihakbalik0 <melihakbalik0@gmail.com>
2026-05-14 15:11:10 +0300

    Delete includes directory

--- includes/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header('Location: ../admin/login.php');
-    exit;
-}
--- includes/site_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Canlı site adresi (sonda / yok; link birleştirirken ekle).
- * Tarayıcıda ?i=1 vb. görünse de canonical için temiz URL kullanılır.
- */
-return [
-    'base_url' => 'https://melihakbalik.free.nf',
-];
